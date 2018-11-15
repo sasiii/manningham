2b5a3508f0ecb51b40e6fd72ef6a7734b455db9f
Hazeer <hazeerahamed@gmail.com>
2018-11-15 12:58:38 +0530

    Added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialcomponentsModule } from './materialcomponents';
-
+import { RouterModule, Routes} from "@angular/router";
 import { AppComponent } from './app.component';
 import { ManSampleCompComponent } from './man-sample-comp/man-sample-comp.component';
+import { TypographyComponent } from './typography/typography.component';
+
+
+
+const appRoutes: Routes = [
+  { path: 'typography', component: TypographyComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ManSampleCompComponent
+    ManSampleCompComponent,
+    TypographyComponent
   ],
   imports: [
     BrowserModule,
