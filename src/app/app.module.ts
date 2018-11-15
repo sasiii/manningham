a939d298e2a4c9d191125c1ba801f0d01a62cc1e
Sasanika <hkasasanika@gmail.com>
2018-11-15 13:59:26 +0530

    variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialcomponentsModule } from './materialcomponents';
-import { RouterModule, Routes} from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
+import { APP_BASE_HREF } from '@angular/common';
 import { AppComponent } from './app.component';
 import { ManSampleCompComponent } from './man-sample-comp/man-sample-comp.component';
 import { TypographyComponent } from './typography/typography.component';
@@ -10,7 +11,10 @@ import { TypographyComponent } from './typography/typography.component';
 
 
 const appRoutes: Routes = [
-  { path: 'typography', component: TypographyComponent}
+  { path: '/typography', component: TypographyComponent },
+
+  { path: '', redirectTo: '/man-sample-comp', pathMatch: 'full' },
+  { path: '**', redirectTo: '/man-sample-comp', pathMatch: 'full' }
 ]
 
 @NgModule({
@@ -22,9 +26,11 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialcomponentsModule
+    MaterialcomponentsModule,
+    // RouterModule.forRoot(appRoutes),
+
   ],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
